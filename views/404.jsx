7390619f4da0c580eb74ebc6825ba48281606518
Matthew Burrows <mburrows0553@sdsu.edu>
2022-03-09 19:28:01 -0800

    Create 404.jsx

---
+++ views/404.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+const Default = require('./layouts/default')
+
+function Four0Four(props){
+    return(
+        <div>
+            <p>You've reached the wrong page</p>
+            <a href='/breads'>Click here to go home</a>
+        </div>
+    )
+}
+
+module.exports = Four0Four
